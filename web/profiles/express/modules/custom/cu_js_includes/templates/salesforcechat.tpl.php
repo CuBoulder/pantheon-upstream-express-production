b5f0f90c7fdd98475003e43fdc60fdbc3efa79f7
AlanBCole <alantuneit@gmail.com>
2023-08-10 00:01:51 -0600

    making sure eye catcher image only shows on desktop browsers

--- web/profiles/express/modules/custom/cu_js_includes/templates/salesforcechat.tpl.php
+++ web/profiles/express/modules/custom/cu_js_includes/templates/salesforcechat.tpl.php
@@ -100,22 +100,29 @@
 </style>
 
 <script type='text/javascript' src='https://service.force.com/embeddedservice/5.0/esw.min.js'></script>
-<script type='text/javascript'>
+<script id="salesforce-chatbot" type='text/javascript'>
     const enableAutoOpen = <?php print check_plain($enable_auto_open); ?>;
     const autoOpenTime = <?php print check_plain($auto_open_time); ?>;
-    if(
-        enableAutoOpen &&
-        !(/Android|webOS|iPhone|iPad|iPod|mobile.+firefox|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
-    ) {
-        embedded_svc.addEventHandler("onSettingsCallCompleted", function(data) {
-            setTimeout(
-                function() {
-                    closeEyeCatcherAndBootstrap();
-                },
-                autoOpenTime * 1000
-            );   
-        });
+    const enableEyeCatcher = <?php print check_plain($enable_eye_catcher); ?>;
+
+    // Auto Open and Eye Catcher should be enabled only on desktop browsers.
+    if(!(/Android|webOS|iPhone|iPad|iPod|mobile.+firefox|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))) {
+        if (enableAutoOpen === 1) {
+            embedded_svc.addEventHandler("onSettingsCallCompleted", function(data) {
+                setTimeout(
+                    function() {
+                        closeEyeCatcherAndBootstrap();
+                    },
+                    autoOpenTime * 1000
+                );   
+            });
+        }
+
+        if (enableEyeCatcher === 1) {
+            addEyeCatcherHtml();
+        }
     }
+
     var initESW = function(gslbBaseURL) {
         embedded_svc.settings.displayHelpButton = true; //Or false
         embedded_svc.settings.language = ''; //For example, enter 'en' or 'en-US'
@@ -140,6 +147,9 @@
 				isOfflineSupportEnabled: true
             }
         );
+
+        const status = data.isAgentAvailable ? "online": "offline";
+        console.log(`onSettingsCallCompleted event was fired. Agent availability status is ${status}.`);
     };
 
     if (!window.embedded_svc) {
@@ -153,23 +163,45 @@
 		initESW('https://service.force.com');
     }
 
-    function closeEyeCatcherAndBootstrap() {
+    function closeimg() {
         const img = document.querySelector("#livechat-eye-catcher");
         if (img != null) {
             img.style.display = "none";
         }
+    }
+
+    function closeEyeCatcherAndBootstrap() {
+        closeimg();
         embedded_svc.bootstrapEmbeddedService();
     }
+
+    function addEyeCatcherHtml() {
+        const eyeCatcher = document.createElement("div");
+        const eyeCatcherCloser = document.createElement("div");
+        const eyeCatcherImgDiv = document.createElement("div");
+        const eyeCatcherImg = document.createElement("img");
+
+        eyeCatcher.id = "livechat-eye-catcher";
+        eyeCatcher.style = "position: fixed; visibility: visible; z-index: 2147483639; background: transparent; border: 0px; padding: 10px 10px 0px 0px; float: left; margin-right: -10px; backface-visibility: hidden; bottom: 59px; right: 13px;"
+
+        eyeCatcherCloser.style = "position: absolute; display: block; top: -5px; right: -3px; width: 16px; line-height: 16px; text-align: center; cursor: pointer; text-decoration: none; color: rgb(0, 0, 0); font-size: 20px; font-family: Arial, sans-serif; border-radius: 50%; background-color: rgba(255, 255, 255, 0.5);";
+        eyeCatcherCloser.addEventListener("click", closeimg);
+        eyeCatcherCloser.textContent = "x";
+        
+        eyeCatcherImgDiv.id = "livechat-eye-catcher-img";
+        eyeCatcherImgDiv.style = "display: block; overflow: hidden; cursor: pointer;"
+        
+        eyeCatcherImg.id = "imagID";
+        eyeCatcherImg.alt = "Chat now";
+        eyeCatcherImg.addEventListener("click", closeEyeCatcherAndBootstrap);
+        eyeCatcherImg.src = "https://cdn.livechat-files.com/api/file/lc/main/12416433/0/ec/3cd23a1c1292c884141874e90a228a2b.png";
+        eyeCatcherImg.style = "display: block; border: 0px; float: right; width: 275px; height: 124px;"
+
+        eyeCatcher.appendChild(eyeCatcherCloser);
+        eyeCatcherImgDiv.appendChild(eyeCatcherImg);
+        eyeCatcher.appendChild(eyeCatcherImgDiv);
+            
+        const chatScript = document.querySelector("#salesforce-chatbot");
+        chatScript.after(eyeCatcher);
+    }
 </script>
-
-
-<?php if ($enable_eye_catcher == 1) : ?>
-    <div id="livechat-eye-catcher" style="position: fixed; visibility: visible; z-index: 2147483639; background: transparent; border: 0px; padding: 10px 10px 0px 0px; float: left; margin-right: -10px; backface-visibility: hidden; bottom: 59px; right: 13px;">
-        <div onclick="closeimg()" style="position: absolute; display: block; top: -5px; right: -3px; width: 16px; line-height: 16px; text-align: center; cursor: pointer; text-decoration: none; color: rgb(0, 0, 0); font-size: 20px; font-family: Arial, sans-serif; border-radius: 50%; background-color: rgba(255, 255, 255, 0.5);">
-                ×
-        </div>
-        <div id="livechat-eye-catcher-img" style="display: block; overflow: hidden; cursor: pointer;">
-            <img id="imagID" alt="Chat now" onclick="closeEyeCatcherAndBootstrap()" src="https://cdn.livechat-files.com/api/file/lc/main/12416433/0/ec/3cd23a1c1292c884141874e90a228a2b.png" style="display: block; border: 0px; float: right; width: 275px; height: 124px;"  />
-        </div>
-    </div>
-<?php endif; ?>
